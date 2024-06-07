1d685ffeaa5d44f2fa7228ac36f040d932761c2c
luis930114 <luis930114@gmail.com>
2024-06-07 08:35:21 -0500

    feat: se agrega el archivo .env al backend

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-//import ollama from 'ollama'
-
-const PORT = 8000
+const PORT = 5000 //8000
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config
-const ollama = require('ollama');
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 
-
+const API_KEY = process.env.API_KEY
 
 
 app.post('/completions', async (req, res) =>{
@@ -30,14 +27,8 @@ app.post('/completions', async (req, res) =>{
        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
        const data = await response.json()
        res.send(data)
-       console.data(data)
     }catch (error){
         console.error(error)
     }
-    /*const response = await ollama.chat({
-        model: 'llama2',
-        messages: [{ role: 'user', content: 'Why is the sky blue?' }],
-      })
-      console.log(response.message.content)*/
 } )
-app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
+app.listen(PORT, '0.0.0.0',() => console.log('Your server is running on PORT ' + PORT))
